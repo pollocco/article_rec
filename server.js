f6224570b64e7aba4351a99719435906f0bb5447
Connor Pollock <pollocco@oregonstate.edu>
2020-05-12 14:03:43 -0500

    Added server routes for form, added server response to form page

--- server.js
+++ server.js
@@ -582,6 +582,139 @@ router.post('/getTopicArticlesFiltered', function(req, res, next){
     }
 })
 
+router.post('/sendArticleForm', function(req, res, next){
+    console.log(req.body)
+    var periodicalName = req.body.periodicalName
+    var title = req.body.title
+    var content = req.body.content
+    var date = req.body.date
+    var url = req.body.url
+    var results = []
+    mysql.pool.query('INSERT INTO Periodicals SET name=? ON DUPLICATE KEY UPDATE Periodicals.periodicalId=Periodicals.periodicalId', [periodicalName], function(error, result){
+        if(error){
+            console.log(error)
+        }
+        else{
+            results.push(result)
+            mysql.pool.query("INSERT INTO Articles (title, content, url, date, periodicalId) VALUES (?, ?, ?, STR_TO_DATE(?, '%Y-%m-%d'), (SELECT periodicalId FROM Periodicals WHERE name=?)) " +
+            'ON DUPLICATE KEY UPDATE Articles.articleId=Articles.articleId;', [title, content, url, date, periodicalName], function(error, result){
+                if(error){
+                    console.log(error)
+                }
+                else{
+                    results.push(result)
+                    res.send(results)
+                }
+            })
+        }
+    })
+})
+
+router.post('/sendAuthorForm', function(req, res, next){
+    var firstName = req.body.firstName
+    var lastName = req.body.lastName
+    mysql.pool.query('INSERT INTO Authors (firstName, lastName) VALUES (?, ?) ' +
+    'ON DUPLICATE KEY UPDATE Authors.authorId=Authors.authorId', [firstName, lastName], function(error, result){
+        if(error){
+            console.log(error)
+        }
+        else{
+            res.send(result)
+        }
+    })
+})
+
+router.post('/sendPeriodicalForm', function(req, res, next){
+    var periodicalName = req.body.periodicalName
+    var periodicalUrl = req.body.periodicalUrl
+    mysql.pool.query('INSERT INTO Periodicals (name, url) VALUES (?, ?) ' +
+    'ON DUPLICATE KEY UPDATE Periodicals.periodicalId=Periodicals.periodicalId', [periodicalName, periodicalUrl], function(error, result){
+        if(error){
+            console.log(error)
+        }
+        else{
+            res.send(result)
+        }
+    } )
+})
+
+router.post('/sendFullArticleForm', function(req, res, next){
+    var title = req.body.title
+    var url = req.body.url
+    var content = req.body.content
+    var firstName = req.body.firstName
+    var lastName = req.body.lastName
+    var periodicalName = req.body.periodicalName
+    var date = req.body.date
+    var topic = req.body.topic
+    var results = []
+    mysql.pool.query('INSERT INTO Topics SET name = ? ' +
+    'ON DUPLICATE KEY UPDATE Topics.topicId=Topics.topicId ', [topic], function(error, result){
+        if(error){
+            console.log(error)
+        }
+        else{
+            results.push(result)
+            mysql.pool.query('INSERT INTO Periodicals SET name=? ' +
+            'ON DUPLICATE KEY UPDATE Periodicals.periodicalId=Periodicals.periodicalId', [periodicalName], function(error, result){
+                if(error){
+                    console.log(error)
+                } else{
+                    results.push(result)
+                    mysql.pool.query("INSERT INTO Articles (title, content, url, date, periodicalId) " +
+                    "VALUES (?, ?, ?, STR_TO_DATE(?, '%Y-%m-%d'), " +
+                    "(SELECT periodicalId FROM Periodicals WHERE Periodicals.name=?)) " +
+                    "ON DUPLICATE KEY UPDATE Articles.articleId=Articles.articleId", [title, content, url, date, periodicalName], function(error, result){
+                        if(error){
+                            console.log(error)
+                        } else{
+                            results.push(result)
+                            mysql.pool.query('INSERT INTO Authors (firstName, lastName) VALUES (?, ?) ' +
+                            'ON DUPLICATE KEY UPDATE Authors.authorId=Authors.authorId', [firstName, lastName], function(error, result){
+                                if(error){
+                                    console.log(error)
+                                }else{
+                                    results.push(result)
+                                    mysql.pool.query('INSERT INTO ArticleTopics (articleId, topicId) VALUES ' +
+                                    '((SELECT articleId FROM Articles WHERE Articles.title=? AND Articles.url=?), ' +
+                                    '(SELECT topicId FROM Topics WHERE Topics.name=?))', [title, url, topic], function(error, result){
+                                        if(error){
+                                            console.log(error)
+                                        } else{
+                                            results.push(result)
+                                            mysql.pool.query('INSERT INTO AuthorArticles (authorId, articleId) VALUES ' +
+                                            '((SELECT authorId FROM Authors WHERE Authors.firstName=? AND Authors.lastName=?), ' +
+                                            '(SELECT articleId FROM Articles WHERE Articles.title = ? AND Articles.url = ?))', 
+                                            [firstName, lastName, title, url], function(error, result){
+                                                if(error){
+                                                    console.log(error)
+                                                } else{
+                                                    results.push(result)
+                                                    mysql.pool.query('INSERT INTO PeriodicalArticles (periodicalId, articleId) VALUES ' +
+                                                    '((SELECT periodicalId FROM Periodicals WHERE Periodicals.name=?), ' +
+                                                    '(SELECT articleId FROM Articles WHERE Articles.title = ? AND Articles.url = ?))', 
+                                                    [periodicalName, title, url], function(error, result){
+                                                        if(error){
+                                                            console.log(error)
+                                                        } else{
+                                                            results.push(result)
+                                                            res.send(results)
+                                                        }
+                                                    })
+                                                }
+                                            })
+                                        }
+                                    })
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    })
+})
+
 router.get('/getUserArticlesHistory', function(req, res, next){
     var userId = req.session.userId;
     mysql.pool.query('SELECT Articles.*, UserArticles.lastViewed, GROUP_CONCAT(Topics.name SEPARATOR " | ") as topic FROM Articles ' +
